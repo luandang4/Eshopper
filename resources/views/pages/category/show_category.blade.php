11fe048120800b1cbc88e2c6d46bd6efce47bc87
luandang4 <luandang4@gmail.com>
2021-06-10 22:47:07 +0700

    show category home

--- resources/views/pages/category/show_category.blade.php
+++ resources/views/pages/category/show_category.blade.php
@@ -7,7 +7,7 @@
 		<div class="product-image-wrapper">
 			<div class="single-products">
 				<div class="productinfo text-center">
-					<img src="{{URL::to('public/upload/product/'.$cate->product_image)}}" alt="" />
+					<img src="{{URL::to('public/upload/product/'.$cate->product_image)}}" width="241.75" height="241.75" alt="" />
 					<h2>{{$cate -> product_price}}</h2>
 					<p>{{$cate -> product_name}}</p>
 					<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
